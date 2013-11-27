1fc9959180f2d42a524a08f6f5f7302d197cd894
Khairul Anwar (kulll) <anwarbaik88@gmail.com>
2013-11-27 14:47:46 +0800

    add fields for finance tab (re #923)

--- ilo/workplan/content/activity.py
+++ ilo/workplan/content/activity.py
@@ -62,4 +62,73 @@ class IActivity(form.Schema, IImageScaleTraversable):
         description=_(''),
         )
 
+    form.fieldset('finance',
+                  label=_(u'Finance'),
+                  fields=['epapo_number',
+                          'rbtc_actemp',
+                          'rbsa_actemp_hq',
+                          'rbsa_regional',
+                          'tc_projects',
+                          'itc_turin',
+                          'eos_company_oie',
+                          'rbtc_oa',
+                          'fund_to_be_determinated',
+                          ],
+                  required=False,
+                  )
+
+    epapo_number = schema.Int(
+        title=_(u'EPA/PO Number'),
+        description=_('e.g. 9119448'),
+        required=False,
+        )
+
+    rbtc_actemp = schema.Float(
+        title=_(u'RBTC ACTEMP'),
+        description=_('e.g. 5500'),
+        required=False,
+        )
+
+    rbsa_actemp_hq = schema.Float(
+        title=_(u'RBSA ACTEMP HQ'),
+        description=_('e.g. 4000'),
+        required=False,
+        )
+
+    rbsa_regional = schema.Float(
+        title=_(u'RBSA Regional'),
+        description=_('e.g. 7300'),
+        required=False,
+        )
+
+    tc_projects = schema.Float(
+        title=_(u'TC projects'),
+        description=_('e.g. 3450'),
+        required=False,
+        )
+
+    itc_turin = schema.Float(
+        title=_(u'ITC Turin'),
+        description=_('e.g. 1200'),
+        required=False,
+        )
+
+    eos_company_oie = schema.Float(
+        title=_(u'EOs, COMPANY, OIE'),
+        description=_('e.g. 7000'),
+        required=False,
+        )
+
+    rbtc_oa = schema.Float(
+        title=_(u'RBTC OA'),
+        description=_('e.g. 5600'),
+        required=False,
+        )
+
+    fund_to_be_determinated = schema.Float(
+        title=_(u'Fund to be determinated'),
+        description=_('e.g. 50000'),
+        required=False,
+        )
+
 alsoProvides(IActivity, IFormFieldProvider)
